916148cb7a4cfc9d654da8dc06dca3daf274ad00
ronyaburaihan <ronyaburaihan@gmail.com>
2023-07-03 08:47:40 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Abu Raihan Rony on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
